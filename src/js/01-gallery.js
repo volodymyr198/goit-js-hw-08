072d13de85e88729d313a70d631a9ee97dd2ef12
volodymyr198 <volodymyr.b.198@gmail.com>
2022-11-13 11:05:20 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,50 +5,19 @@ import { galleryItems } from './gallery-items';
 
 const galleryEl = document.querySelector('.gallery');
 
-const onSelectImg = event => {
-  event.preventDefault();
-  const largeImg = basicLightbox.create(
-    `
-		<img
-        class="gallery__image"
-        src="${event.target.dataset.source}"
-        data-source="large-image.jpg"
-        alt="Image description"
-    />
-	`,
-    {
-      onShow: largeImg => window.addEventListener('keydown', onEscPressClose),
-      onClose: largeImg =>
-        window.removeEventListener('keydown', onEscPressClose),
-    }
-  );
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-  largeImg.show();
-
-  function onEscPressClose(event) {
-    if (event.code === 'Escape') {
-      largeImg.close();
-    }
-  }
-};
-galleryEl.addEventListener('click', onSelectImg);
-
 const createEl = galleryItems
   .map(items => {
-    return `<div class="gallery__item">
-        <a class="gallery__link" href="${items.original}">
-            <img
-                class="gallery__image"
-                src="${items.preview}"
-                data-source="${items.original}"
-                alt="${items.description}"
-            />
-        </a>
-    </div>`;
+    return `<a class="gallery__item" href="${items.original}">
+    <img class="gallery__image" src="${items.preview}" alt="${items.description}" />
+</a>`;
   })
   .join('');
 galleryEl.insertAdjacentHTML('beforeEnd', createEl);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  animationSpeed: 400,
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,7 @@
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
-const player = new Player('handstick', {
-  id: 19231868,
-  width: 640,
-});
+// const player = new Player('handstick', {
+//   id: 19231868,
+//   width: 640,
+// });
