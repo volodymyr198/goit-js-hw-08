65674a12a52cdf93281b1b8a2222f2b361745f0e
volodymyr198 <volodymyr.b.198@gmail.com>
2022-11-15 18:47:46 +0200

    fixed task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,8 +4,8 @@ const form = document.querySelector('form');
 const input = document.querySelector('input');
 const textarea = document.querySelector('textarea');
 
-const formData = {};
 const STORAGE_KEYS = 'feedback-form-state';
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEYS)) || {};
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormData, 500));
@@ -19,6 +19,7 @@ function onFormSubmit(event) {
 
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEYS);
+  formData = {};
 }
 
 function onFormData(event) {
@@ -31,7 +32,7 @@ function populateData() {
   const savedData = JSON.parse(localStorage.getItem(STORAGE_KEYS));
 
   if (savedData) {
-    input.value = savedData.email;
-    textarea.value = savedData.message;
+    input.value = savedData.email || '';
+    textarea.value = savedData.message || '';
   }
 }
